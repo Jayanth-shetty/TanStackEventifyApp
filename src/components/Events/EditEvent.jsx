73213445ad39fb34b0ccefc090765624c449444f
jayanth <jayanthshetty660@gmail.com>
2025-10-15 22:00:04 +0530

    replaced usequery with action and loaders

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,54 +1,55 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { Link, redirect, useNavigate, useNavigation, useParams, useSubmit } from "react-router-dom";
+import {  useQuery } from "@tanstack/react-query";
 import { fetchEvent, updateEvent, queryClient } from "../../util/http.js";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+// import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
+  const {state}=useNavigation();
+  const submit=useSubmit();
   const params = useParams();
   const navigate = useNavigate();
 
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async (data) => {
-      const newEvent = data.event;
-      await queryClient.cancelQueries({ queryKey: ["events", params.id] });
-      const previousEvent = queryClient.getQueryData(["events", params.id]);
-      queryClient.setQueryData(["events", params.id], newEvent);
-      return { previousEvent };
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(["events", params.id], context.previousEvent);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries(["events", params.id]);
-    },
-  });
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async (data) => {
+  //     const newEvent = data.event;
+  //     await queryClient.cancelQueries({ queryKey: ["events", params.id] });
+  //     const previousEvent = queryClient.getQueryData(["events", params.id]);
+  //     queryClient.setQueryData(["events", params.id], newEvent);
+  //     return { previousEvent };
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(["events", params.id], context.previousEvent);
+  //   },
+  //   onSettled: () => {
+  //     queryClient.invalidateQueries(["events", params.id]);
+  //   },
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
-    navigate("../");
+    submit(formData,{method:'PUT'})
   }
 
   function handleClose() {
     navigate("../");
   }
   let content;
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
+  // if (isPending) {
+  //   content = (
+  //     <div className="center">
+  //       <LoadingIndicator />
+  //     </div>
+  //   );
+  // }
 
   if (isError) {
     content = (
@@ -73,15 +74,31 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
+        {state==='submitting'?<p>Submitting data......</p>:<><Link to="../" className="button-text">
           Cancel
         </Link>
         <button type="submit" className="button">
           Update
-        </button>
+        </button></>}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content && content}</Modal>;
+}
+
+
+export function loader({params}){
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  })
+}
+
+export async function action({request,params}){
+ const formData=await request.formData();
+ const updatedData=Object.fromEntries(formData);
+ await updateEvent({id:params.id,event:updatedData})
+ await queryClient.invalidateQueries(['events']);
+ return redirect('../')
 }
